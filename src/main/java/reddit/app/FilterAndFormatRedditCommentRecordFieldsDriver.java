b5a21e00ed65135d126c2cc4bc1e3ba82ab9b75e
raholm <rasmus.holm723@gmail.com>
2016-12-31 00:01:57 +0100

    Removed a few redundancies.

--- src/main/java/reddit/app/FilterAndFormatRedditCommentRecordFieldsDriver.java
+++ src/main/java/reddit/app/FilterAndFormatRedditCommentRecordFieldsDriver.java
@@ -1,19 +1,15 @@
 package reddit.app;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-import org.codehaus.jettison.json.JSONException;
 
 public class FilterAndFormatRedditCommentRecordFieldsDriver
   extends Configured implements Tool {
