53666774c702b0b5387abfd1fff41a662efa0442
raholm <rasmus.holm723@gmail.com>
2016-12-31 16:14:48 +0100

    Simple hadoop streaming test script with python/java classes.

--- run_streaming.sh
+++ run_streaming.sh
@@ -5,7 +5,11 @@ rm -rf ./output/streaming
 hadoop jar $HADOOP_PATH/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
        -conf ./conf/hadoop/hadoop-local.xml \
        -libjars ./target/reddit-1.0-SNAPSHOT.jar \
+       -files ./src/main/python/reddit/app/identity_reducer.py \
        -input ./input/reddit_comments.json \
        -output ./output/streaming \
        -mapper reddit.app.FilterAndFormatRedditCommentRecordFieldsMapperOld \
-       -reducer ./src/main/python/reddit/app/identity_reducer.py
+       -reducer identity_reducer.py
+
+echo "Output:"
+cat ./output/streaming/part-00000
--- src/main/python/reddit/app/identity_reducer.py
+++ src/main/python/reddit/app/identity_reducer.py
@@ -1,6 +1,16 @@
 #!/usr/local/bin/python3.5
 
 import sys
+import json
+import time
+
+thread_ids = []
 
 for line in sys.stdin:
-    print(line)
+    obj = json.loads(line)
+    try:
+        thread_ids.append(obj["threadId"][3:])
+    except KeyError:
+        continue
+
+[print(thread_id) for thread_id in thread_ids]
