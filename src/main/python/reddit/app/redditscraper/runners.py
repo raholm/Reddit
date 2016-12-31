5c0ba5462ad705bf4b3c98009b0f2770ba03cf68
raholm <rasmus.holm723@gmail.com>
2016-12-31 18:19:23 +0100

    Added a little documentation.

--- src/main/python/reddit/app/redditscraper/runners.py
+++ src/main/python/reddit/app/redditscraper/runners.py
@@ -5,10 +5,23 @@ from twisted.internet import reactor
 from redditscraper.spiders import RedditThreadSpider
 
 class RedditSpiderRunner(object):
+    """
+    Base class for running a Reddit spider.
+    """
     def run(self):
         raise NotImplemented
 
 class RedditThreadSpiderRunner(object):
+    """
+    Used for fetching thread information by calling the .run() method.
+
+    Parameters
+    -----------
+    subreddit_thread_ids: dict of list
+    A dictionary where the key is the subreddit and value is a list of thread ids to fetch data from.
+    Format: {"subreddit1": ["thread_id1", "thread_id2", ...], "subreddit2": [...], ...}
+    """
+
     def __init__(self, subreddit_thread_ids):
         self.subreddit_thread_ids = subreddit_thread_ids
 
